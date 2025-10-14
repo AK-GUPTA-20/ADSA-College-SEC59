7f479f4c007ad9d8e860a0c7c04a6f14e9b770de
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-14 22:10:22 +0530

    Create 4. SortColors.java

---
+++ 2-Pointer.java/4. SortColors.java
@@ -0,0 +1,26 @@
+/*
+75. Sort Colors
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,0);
+        map.put(1,0);
+        map.put(2,0);
+        for(int num : nums){
+            map.put(num , map.get(num)+1);
+        }
+
+        int start = 0;
+        for(int i=0 ; i<3 ; i++){
+            int freq = map.get(i);
+            for(int j=start ; j<freq+start ; j++){
+                nums[j] = i;
+            }
+            start += freq ;
+        }
+    }
+}
