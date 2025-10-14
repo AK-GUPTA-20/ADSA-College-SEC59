137b7e99e683d746780b297658fd7e7592575e91
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-14 22:09:04 +0530

    Create 3. IntersectionofTwoArrays.java

---
+++ 2-Pointer.java/3. IntersectionofTwoArrays.java
@@ -0,0 +1,45 @@
+/*
+349. Intersection of Two Arrays
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+*/
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> newArray = new ArrayList<>();
+        Arrays.sort(nums1); 
+        Arrays.sort(nums2);
+        int i = 0, j = 0;
+        int n1 = nums1.length, n2 = nums2.length;
+
+        while (i < n1 && j < n2) {
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            
+            else{
+                if(newArray.size()==0 || newArray.get(newArray.size()-1)!=nums1[i]){
+                    newArray.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+            
+        }
+
+
+        // Convert List to int[]
+        int[] result = new int[newArray.size()];
+        for (int k = 0; k < newArray.size(); k++) {
+            result[k] = newArray.get(k);
+        }
+
+ 
+        return result;
+    }
+}
