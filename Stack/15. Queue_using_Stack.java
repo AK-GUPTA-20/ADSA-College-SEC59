a8ba97592e6f0aaa4ca88ee38732a0e67069ee1e
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-15 21:55:30 +0530

    Create 15. Queue_using_Stack.java

---
+++ Stack/15. Queue_using_Stack.java
@@ -0,0 +1,40 @@
+/*
+232. Implement Queue using Stacks  (https://leetcode.com/problems/implement-queue-using-stacks/description/)
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+*/
+
+class MyQueue {
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
