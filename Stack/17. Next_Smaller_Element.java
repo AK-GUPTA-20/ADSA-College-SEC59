7c3330530b82ab0276bcdc804e55564d79348345
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-17 17:40:18 +0530

    Create 17. Next_Smaller_Element.java

---
+++ Stack/17. Next_Smaller_Element.java
@@ -0,0 +1,28 @@
+/*
+Next Smaller Element   (https://www.geeksforgeeks.org/problems/immediate-smaller-element1142/1)
+Input: arr[] = [4, 8, 5, 2, 25]
+Output: [2, 5, 2, -1, -1]
+*/
+class Solution {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        int n = arr.length;
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) result.add(-1);
+
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            
+            while (!st.isEmpty() && st.peek() >= arr[i]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) {
+                result.set(i, st.peek());
+            }
+            st.push(arr[i]);
+        }
+
+        return result;
+    }
+}
