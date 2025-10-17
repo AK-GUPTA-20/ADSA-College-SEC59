236dcecac6c9b6063ed2133a1e985ef6217c06b9
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-10-17 17:43:13 +0530

    Create 18. Largest Rectangle in Histogram
    Implemented a solution for calculating the largest rectangle area in a histogram using a stack.

---
+++ Stack/18. Largest_Rectangle_Histogram.java
@@ -0,0 +1,34 @@
+/*
+84. Largest Rectangle in Histogram  (https://leetcode.com/problems/largest-rectangle-in-histogram/description/)
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+*/
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        int n = heights.length;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i <= n; i++) {
+            // Use 0 height for the end of array to clear out remaining bars
+            int currHeight = (i == n) ? 0 : heights[i];
+            
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width;
+                
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+                
+                maxArea = Math.max(maxArea, height * width);
+            }
+            
+            stack.push(i);
+        }
+        
+        return maxArea;
+    }
+}
