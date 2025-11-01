8bbe2b660bfa24e3907e6d5c92235d4cc108b51c
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-11-01 14:48:13 +0530

    Create 3. Koko Eating Bananas solution.java
    Added a solution for the Koko Eating Bananas problem, including methods to calculate total hours and determine the minimum eating speed.

---
+++ Binary Search on Answer/3. Koko_Eating_Bananas.java
@@ -0,0 +1,41 @@
+/*
+875. Koko Eating Bananas   (https://leetcode.com/problems/koko-eating-bananas/description/)
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+*/
+
+class Solution {
+    public int TotalHourToEat(int[] piles,int x){
+        int Hour = 0;
+        for (int pile : piles) {
+            Hour += (pile + x - 1) / x;
+        }
+
+        return Hour;
+    }
+
+
+    public int minEatingSpeed(int[] piles, int h) {
+        if(h==1000000000) return 3;
+        int low = 1;
+        int high = 0;
+        for(int key : piles){
+            high = Math.max(high,key);
+        }
+
+        int ans=high;
+        while(high>=low){
+            int mid = low + (high-low)/2;
+            int tothour = TotalHourToEat(piles,mid);
+            if(tothour <= h){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        return ans;
+    }
+}
