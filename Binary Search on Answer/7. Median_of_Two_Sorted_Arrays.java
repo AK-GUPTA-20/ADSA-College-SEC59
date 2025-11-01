b57c849aeed8bf8e28a5ec5749706de65f1da478
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-11-01 14:55:58 +0530

    Create 7. Median_of_Two_Sorted_Arrays.java

---
+++ Binary Search on Answer/7. Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,40 @@
+/*
+4. Median of Two Sorted Arrays         (https://leetcode.com/problems/median-of-two-sorted-arrays/description/)
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+*/
+
+class Solution {
+    int[] Merge(int[] nums1, int[] nums2){
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int total = n1 + n2;
+        int mid = total / 2 + 1; 
+        int[] merged = new int[mid];
+
+        int i = 0, j = 0, k = 0;
+        while (k < mid) {
+            if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        return merged;
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double median;
+        int[] arr = Merge(nums1, nums2);
+        int total = nums1.length + nums2.length;
+
+        if (total % 2 == 0) {
+            median = (arr[arr.length - 1] + arr[arr.length - 2]) / 2.0;
+        } else {
+            median = arr[arr.length - 1];
+        }
+
+        return median;
+    }
+}
