74e47e97e6e3ed23425e991e02ddf0c9ffc1766b
Akshat Gupta <162670395+AK-GUPTA-20@users.noreply.github.com>
2025-11-01 14:54:05 +0530

    Create 6. solution for minimum days to make bouquets

---
+++ Binary Search on Answer/6. Minimum_Number_of_Days_to_Make_Bouquets.java
@@ -0,0 +1,49 @@
+/*
+1482. Minimum Number of Days to Make m Bouquets  ()
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 1
+Output: 3
+*/
+
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length) {
+            return -1;
+        }
+
+        int low = 1, high = (int) 1e9;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (isPossibleBouquets(bloomDay, m, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        };
+
+        return low;
+    }
+    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
+        int total = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            int count = 0;
+            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
+                count++;
+                i++;
+            }
+
+            if (count == k) {
+                total++;
+                i--;
+            }
+
+            if (total >= m) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
